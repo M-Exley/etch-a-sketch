29192ad2f86fde6c57d5a77a8fb3e15f5c79e0fd
Macaylen <macaylen.exley@gmail.com>
2024-10-31 13:12:35 +0100

    modified original

--- script.js
+++ script.js
@@ -1,34 +1,42 @@
 'use strict'
-const gridSize = Number(prompt("Enter number of squares per axis, 16 or less:"));
-let x = gridSize;
-console.log(x);
 
-function draw (numberOfSquares) {
-    for(x=0; x<=numberOfSquares; x++) {
-    const container = document.querySelector('.container');
-    const newDivOne = document.createElement('div');
-    newDivOne.className = 'new-div-one';
-    newDivOne.textContent = x;
-    container.appendChild(newDivOne);
-    newDivOne.addEventListener('mouseenter', function(e) {
-        let target = newDivOne;
-        console.log(e.target);
-        if (e.target) {
-            target.style.backgroundColor = "red";
+const container = document.querySelector('.container');
+
+
+
+function createGrid(gridInput){
+    for(let i = 0; i < gridInput; i++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+        container.appendChild(row);
+        // row.textContent = i;
+
+        for(let j=0; j < gridInput; j++) {
+        const square = document.createElement("div")
+        square.classList.add("square");
+        square.textContent = j;
+        row.appendChild(square);
+        
+    }
+}
+
+checkChild();
+}
+
+function checkChild() {
+    if (container.hasChildNodes) {
+        console.log("YES")
+    }
+    
+    const childNodes = document.querySelectorAll('div');
+    childNodes.forEach((child) => {
+        child.addEventListener('mouseleave', function(e) {
+            console.log(e.target);
+            e.target.style.backgroundColor = "red";
             
-        }
+        })
     })
 }
-}
 
-function getContainerDimensions () {
-    const container = document.querySelector('.container');
-    let height = container.getBoundingClientRect().height;
-    let width = container.getBoundingClientRect().width;
-    let availableSpace = (height * width) / 16;
-    console.log(availableSpace);
-     
-    
-}
-draw(gridSize - 1);
-getContainerDimensions();
+
+createGrid(16);
