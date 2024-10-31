83e3cd09503d7b98d6c0bb59b714dd186b23a328
Macaylen <macaylen.exley@gmail.com>
2024-10-31 08:32:05 +0100

    grid done

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # etch-a-sketch
+
+## CSS-based project
---
+++ script.js
@@ -0,0 +1,15 @@
+'use strict'
+
+// const newDivOne = document.createElement('div');
+// newDivOne.className = 'new-div-one';
+// container.appendChild(newDivOne);
+
+
+for(let x=0; x<16; x++) {
+    const container = document.querySelector('.container');
+    const newDivOne = document.createElement('div');
+    newDivOne.className = 'new-div-one';
+    container.appendChild(newDivOne);
+}
+
+
