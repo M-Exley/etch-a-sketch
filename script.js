0604703fe1e0b5036da0e90809071d3f40db1b6a
Macaylen <macaylen.exley@gmail.com>
2024-10-31 10:15:12 +0100

    testing dimensions

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
 'use strict'
+const gridSize = Number(prompt("Enter number of squares per axis, 16 or less:"));
+let x = gridSize;
+console.log(x);
 
-// const newDivOne = document.createElement('div');
-// newDivOne.className = 'new-div-one';
-// container.appendChild(newDivOne);
-
-
-for(let x=0; x<16; x++) {
+function draw (numberOfSquares) {
+    for(x=0; x<=numberOfSquares; x++) {
     const container = document.querySelector('.container');
     const newDivOne = document.createElement('div');
     newDivOne.className = 'new-div-one';
+    newDivOne.textContent = x;
     container.appendChild(newDivOne);
+    newDivOne.addEventListener('mouseenter', function(e) {
+        let target = newDivOne;
+        console.log(e.target);
+        if (e.target) {
+            target.style.backgroundColor = "red";
+            
+        }
+    })
+}
 }
 
-
+function getContainerDimensions () {
+    const container = document.querySelector('.container');
+    let height = container.getBoundingClientRect().height;
+    let width = container.getBoundingClientRect().width;
+    let availableSpace = (height * width) / 16;
+    console.log(availableSpace);
+     
+    
+}
+draw(gridSize - 1);
+getContainerDimensions();
