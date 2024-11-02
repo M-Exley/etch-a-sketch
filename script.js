7259f570ad2959a2a9c5f8a3ad93a6617ba62529
Macaylen <macaylen.exley@gmail.com>
2024-11-02 11:58:58 +0100

    updated logic

--- script.js
+++ script.js
@@ -9,12 +9,12 @@ function createGrid(gridInput){
         const row = document.createElement('div');
         row.classList.add('row');
         container.appendChild(row);
-        // row.textContent = i;
 
         for(let j=0; j < gridInput; j++) {
-        const square = document.createElement("div")
-        square.classList.add("square");
-        square.textContent = j;
+            const square = document.createElement("div")
+            square.classList.add("square");
+            square.classList.add('hover');
+        // square.textContent = j;
         row.appendChild(square);
         
     }
@@ -23,18 +23,21 @@ function createGrid(gridInput){
 checkChild();
 }
 
-function checkChild() {
-    if (container.hasChildNodes) {
-        console.log("YES")
-    }
-    
-    const childNodes = document.querySelectorAll('div');
+function checkChild() {    
+    const childNodes = document.querySelectorAll("div.hover");
+    let count = 0;
+    const countNum = document.querySelector(".count");
     childNodes.forEach((child) => {
-        child.addEventListener('mouseleave', function(e) {
+        child.addEventListener('mouseenter', function(e) {
             console.log(e.target);
-            e.target.style.backgroundColor = "red";
+            // e.stopPropagation();
+            e.target.style.backgroundColor = "tomato";
+            // e.target.style.border = "yellow 0.1px solid";
+            count++;
+            countNum.textContent = count;
+            // console.log(count);
             
-        })
+        }, {capture: false})
     })
 }
 
