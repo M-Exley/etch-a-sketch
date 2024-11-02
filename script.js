ed0c47e7fdf9284165140d76381802e21c81d3f4
Macaylen <macaylen.exley@gmail.com>
2024-11-02 18:56:54 +0100

    buttons added

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 'use strict'
 
 const container = document.querySelector('.container');
-
-
+const buttonIncrease = document.querySelector('.increase');
+const buttonDecrease = document.querySelector('.decrease');
 
 function createGrid(gridInput){
     for(let i = 0; i < gridInput; i++) {
@@ -14,13 +14,36 @@ function createGrid(gridInput){
             const square = document.createElement("div")
             square.classList.add("square");
             square.classList.add('hover');
-        // square.textContent = j;
+        square.textContent = j;
         row.appendChild(square);
         
     }
-}
+    }
 
-checkChild();
+    function gridChangeSize(e){
+    
+    let countBtnIncrease = gridInput;
+    let countBtnDecrease = gridInput;
+    if (e.target === buttonIncrease){
+        countBtnIncrease ++;
+        console.log(countBtnIncrease);
+        createGrid(gridInput + 1);
+        // return gridInput + 1;
+    }
+        else if (e.target === buttonDecrease) {
+        countBtnDecrease--;
+        console.log(countBtnIncrease);
+        return gridInput - 1;
+        // createGrid(countBtnDecrease)
+        }
+    console.log(countBtnIncrease)
+    console.log(countBtnDecrease)
+    // countBtnIncrease = createGrid();
+    // createGrid();
+    }
+    checkChild();
+    buttonIncrease.addEventListener('click', gridChangeSize)
+    buttonDecrease.addEventListener('click', gridChangeSize)
 }
 
 function checkChild() {    
@@ -29,17 +52,18 @@ function checkChild() {
     const countNum = document.querySelector(".count");
     childNodes.forEach((child) => {
         child.addEventListener('mouseenter', function(e) {
-            console.log(e.target);
+            // console.log(e.target);
             // e.stopPropagation();
             e.target.style.backgroundColor = "tomato";
             // e.target.style.border = "yellow 0.1px solid";
             count++;
             countNum.textContent = count;
-            // console.log(count);
+            if (count === 256) {
+                countNum.textContent = "DONE";
+            }
             
-        }, {capture: false})
+        }, {capture: false, once: true})
     })
 }
-
 
 createGrid(16);
